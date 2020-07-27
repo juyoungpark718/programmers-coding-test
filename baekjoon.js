c9fd9c526a949b2a09bf71fb55b2220258eee9e1
박주영 <juyoung7018.park@gmail.com>
2020-07-27 23:33:19 +0900

    9205-beer 1(not finished)

--- baekjoon.js
+++ baekjoon.js
@@ -1,109 +1,77 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-// const input = fs.readFileSync("./stdin").toString().trim().split("\n");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("./stdin").toString().trim().split("\n");
 
-const dx = [1, 0, -1, 0];
-const dy = [0, 1, 0, -1];
+const testCaseCount = Number(input[0].trim());
 
-const [rowSize, columnSize] = input[0]
-  .trim()
-  .split(" ")
-  .map((element) => Number(element));
-
-const print = (arr) => {
-  arr.forEach((els) => {
-    let str = "";
-    els.forEach((e) => (str += e));
-    console.log(str);
-  });
-};
-
-const getGlacierLand = (inputs) => {
-  const graciersLand = [];
-  const graciers = [];
-  inputs.forEach((input, row) => {
-    graciersLand.push(
-      input
-        .trim()
-        .split(" ")
-        .map((gracier, column) => {
-          if (gracier > 0) graciers.push([column, row]);
-          return Number(gracier);
-        })
-    );
-  });
-  return [graciersLand, graciers];
-};
-
-const [gracierLand, graciers] = getGlacierLand(input.slice(1, input.length));
-
-const meltGracier = (land, graciers) => {
-  const tempLand = land.map((element) => [...element]);
-  graciers.forEach((gracier) => {
-    const [x, y] = gracier;
-    // 상하좌우
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-      if (
-        nx < 0 ||
-        ny < 0 ||
-        nx >= columnSize ||
-        ny >= rowSize ||
-        land[ny][nx] !== 0
-      )
-        continue;
-      tempLand[y][x] === 0 ? (tempLand[y][x] = 0) : tempLand[y][x]--;
-    }
-  });
-  return tempLand;
-};
-
-const countSection = (land, graciers) => {
-  const visited = Array.from(Array(rowSize), () => Array(columnSize));
-  let section = 0;
-  graciers.forEach((gracier) => {
-    const queue = [gracier];
-    if (visited[gracier[1]][gracier[0]] || land[gracier[1]][gracier[0]] === 0)
-      return;
-    visited[gracier[1]][gracier[0]] = true;
-    while (queue.length) {
-      const [x, y] = queue.shift();
-      //상하좌우
-      for (let i = 0; i < 4; i++) {
-        const nx = x + dx[i];
-        const ny = y + dy[i];
-        if (
-          nx < 0 ||
-          ny < 0 ||
-          nx >= columnSize ||
-          ny >= rowSize ||
-          visited[ny][nx] ||
-          land[ny][nx] === 0
-        )
-          continue;
-        visited[ny][nx] = true;
-        queue.push([nx, ny]);
+const getTestCases = (inputs, testCaseCount) => {
+  const cases = [];
+  let index = 0;
+  for (let count = 0; count < testCaseCount; count++) {
+    const convenienceCount = Number(inputs[index].trim());
+    let tempIndex = index;
+    index += convenienceCount + 2 + 1;
+    const testCase = {
+      currentLocation: null,
+      destination: null,
+      convenience: [],
+    };
+    for (let location = tempIndex + 1; location < index; location++) {
+      if (location === tempIndex + 1)
+        testCase.currentLocation = inputs[location]
+          .trim()
+          .split(" ")
+          .map((element) => Number(element));
+      else if (location === index - 1)
+        testCase.destination = inputs[index - 1]
+          .trim()
+          .split(" ")
+          .map((element) => Number(element));
+      else {
+        testCase.convenience.push(
+          inputs[location]
+            .trim()
+            .split(" ")
+            .map((element) => Number(element))
+        );
       }
     }
-    section++;
-  });
-  return section;
-};
-
-const excuteBFS = () => {
-  let land = gracierLand.map((element) => [...element]);
-  let tempLand = land.map((element) => [...element]);
-  let section = countSection(land, graciers);
-  let time = 0;
-  while (section < 2) {
-    tempLand = meltGracier(tempLand, graciers);
-    time++;
-    section = countSection(tempLand, graciers);
-    if (section === 0) break;
+    cases.push(testCase);
   }
-  return section === 0 ? 0 : time;
+  return cases;
 };
 
-const result = excuteBFS();
-console.log(result);
+const testCases = getTestCases(input.slice(1, input.length), testCaseCount);
+
+const executeBFS = () => {
+  testCases.forEach((testCase) => {
+    const { currentLocation, destination } = testCase;
+    let convenience = testCase.convenience;
+    const [endX, endY] = destination;
+    convenience.push(destination);
+    const queue = [currentLocation];
+    while (queue.length) {
+      const [startX, startY] = queue.shift();
+      if (endX === startX && endY === startY) {
+        console.log("happy");
+        return;
+      }
+
+      let i = 0;
+      while (i !== convenience.length) {
+        const [nx, ny] = [Math.abs(startX - convenience[i][0]), Math.abs(startY - convenience[i][1])];
+        if (nx + ny <= 1000) {
+          queue.push([convenience[i][0], convenience[i][1]]);
+          convenience = convenience.filter((_, id) => id !== i);
+          i = 0;
+          break;
+        } else {
+          i++;
+        }
+      }
+    }
+    console.log("sad");
+  });
+};
+
+executeBFS();
